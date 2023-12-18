dfc91a7ebacc546e69be33dc8879aa754a3e9620
MDM952 <96263441+MDM952@users.noreply.github.com>
2023-12-18 15:52:50 +0100

    Lasso
    Added Lasso description

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,14 +1,20 @@
+// Get all the images with the class name "myImg"
+var images = document.getElementsByClassName("myImg");
+
 // Get the modal
 var modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
+
+// Loop through all the images and add an onclick event listener
+for (var i = 0; i < images.length; i++) {
+  images[i].onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
 }
 
 // Get the <span> element that closes the modal
